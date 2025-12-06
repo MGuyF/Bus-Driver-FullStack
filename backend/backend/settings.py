e7ea6fc01dce2094f8524c36fb75b77b275c21bb
MGuyF <2000291gf@gmail.com>
2025-12-06 22:19:24 +0200

    feat(debug): Make DEBUG configurable via env variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tajyq^amq%6(!i^c%la(_t-tt3#*zwl!#w-u-)g-*xjh5*7v#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Set DEBUG = True only for development/debugging
+# The value is read from an environment variable for flexibility
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['busdriver-api.onrender.com', 'localhost', '127.0.0.1']
 
